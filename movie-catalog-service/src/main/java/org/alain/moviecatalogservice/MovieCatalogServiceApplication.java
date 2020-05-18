1391c80bd72f6608dd49338eb4523eb6870c1950
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 10:58:24 +0200

    Adding eureka client to services applications

--- movie-catalog-service/src/main/java/org/alain/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/org/alain/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,11 +3,13 @@ package org.alain.moviecatalogservice;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
     public static void main(String[] args) {
