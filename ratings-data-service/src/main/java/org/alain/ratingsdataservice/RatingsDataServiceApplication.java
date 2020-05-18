c7f079489f63d391e90e6a5d777c79b8fe3ea2c8
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 08:16:58 +0200

    Initialize 3 spring microservices

---
+++ ratings-data-service/src/main/java/org/alain/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package org.alain.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApplication.class, args);
+    }
+
+}
