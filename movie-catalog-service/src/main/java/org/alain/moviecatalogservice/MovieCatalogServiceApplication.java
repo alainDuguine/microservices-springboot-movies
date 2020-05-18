a58fafcae38a74928ae8eff032466fed9b3372ff
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 11:27:56 +0200

    Adding eureka client to services applications

--- movie-catalog-service/src/main/java/org/alain/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/org/alain/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,6 +3,7 @@ package org.alain.moviecatalogservice;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class MovieCatalogServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- movie-catalog-service/src/main/java/org/alain/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/alain/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,9 +2,9 @@ package org.alain.moviecatalogservice.resources;
 
 import org.alain.moviecatalogservice.models.CatalogItem;
 import org.alain.moviecatalogservice.models.Movie;
-import org.alain.moviecatalogservice.models.Rating;
 import org.alain.moviecatalogservice.models.UserRatings;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.Arrays;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,6 +23,8 @@ public class MovieCatalogResource {
     private final RestTemplate restTemplate;
     private final WebClient.Builder webClientbuilder;
 
+    private DiscoveryClient discoveryClient;
+
     public MovieCatalogResource(RestTemplate restTemplate, @Qualifier("webClientBuilder") WebClient.Builder webClientbuilder) {
         this.restTemplate = restTemplate;
         this.webClientbuilder = webClientbuilder;
@@ -34,11 +34,11 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
         UserRatings ratings = restTemplate.getForObject(
-                "http://localhost:8083/ratingsdata/users/" + userId,
+                "http://ratings-data-service/ratingsdata/users/" + userId,
                 UserRatings.class);
 
         return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "Test",rating.getRating());
         }).collect(Collectors.toList());
     }
