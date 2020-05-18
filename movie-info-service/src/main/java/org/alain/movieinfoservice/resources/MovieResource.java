03efadd738e67bf485b5e6beec4c62464134406e
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 15:02:14 +0200

    Adding external caall to movieDb api with apiKey & restTemplate

--- movie-info-service/src/main/java/org/alain/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/org/alain/movieinfoservice/resources/MovieResource.java
@@ -1,17 +1,30 @@
 package org.alain.movieinfoservice.resources;
 
 import org.alain.movieinfoservice.model.Movie;
+import org.alain.movieinfoservice.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+    public MovieResource(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        return new Movie(movieId, "Test name");
+        MovieSummary summary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, summary.getTitle(), summary.getOverview());
     }
 }
