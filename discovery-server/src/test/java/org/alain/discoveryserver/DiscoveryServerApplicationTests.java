04f97c3a93a5df10fd5bcc5f190631e7b3cf8234
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 10:15:58 +0200

    Adding microservice discovery-server

---
+++ discovery-server/src/test/java/org/alain/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alain.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
