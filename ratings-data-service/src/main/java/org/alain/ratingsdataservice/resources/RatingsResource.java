9d379a5c1ffea9980b0125c3ee5f238e36da3912
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-18 08:22:50 +0200

    Adding resource and model for rating ms

---
+++ ratings-data-service/src/main/java/org/alain/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,20 @@
+package org.alain.ratingsdataservice.resources;
+
+import org.alain.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @GetMapping("/{movieId}")
+    public Rating getCatalog(@PathVariable("movieId") String movieId){
+
+        return new Rating(movieId,4);
+
+    }
+}
